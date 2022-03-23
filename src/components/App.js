c5662b35231cf819e51bdf1bd00e332b75735b17
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-03-23 14:41:49 -0300

    layout root ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import '../css/reset.css';
-import '../css/style.css';
+import '../css/style.scss';
+
+import Movie from '../assets/movie-test.jpeg';
 
 function App() {
     return (
-        <h1>oi</h1>
+        <>
+        <header>
+            CINEFLEX
+        </header>
+        <main className="main">
+            <p>Selecione o filme</p>
+            <div className="main__movies">
+                <img src={Movie} alt="img-movie"/>
+                <img src={Movie} alt="img-movie"/>
+                <img src={Movie} alt="img-movie"/>
+                <img src={Movie} alt="img-movie"/>
+            </div>
+        </main>
+        </>
     )
 }
 

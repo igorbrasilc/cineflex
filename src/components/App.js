710abe5dce98c78da2859906a31542c788a1530b
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-03-23 08:52:54 -0300

    primeiro commit, apenas com setting basicas

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import '../css/reset.css';
+import '../css/style.css';
+
+function App() {
+    return (
+        <h1>oi</h1>
+    )
+}
+
+export default App;
